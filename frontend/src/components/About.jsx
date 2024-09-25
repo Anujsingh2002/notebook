370730250cdcd8c3e892a93d33dd7c39d273dcf8
Anuj Singh <anujpratapsinghchbs@gmail.com>
2024-09-25 22:28:37 +0530

    Added About.jsx

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -5,7 +5,29 @@ const About = () => {
   
 
   return (
-    <div>About {}</div>
+    <div className='d-flex '>
+      <div >
+        <img style={{
+        width:'100%',
+        height: '500px'
+      }} src="https://images.pexels.com/photos/733857/pexels-photo-733857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+      </div>
+      <div >
+         <h1 >NOTEBOOK</h1>
+         <p>
+          This is a Notebook website where people can create their personal notes for free.
+         </p>
+         <p>
+         This service is provided by Anuj Pratap Singh.
+         </p>
+         <p>
+          <h5>
+            Contact us: 
+          </h5>
+          aps***@gmail.com
+         </p>
+      </div>
+    </div>
   )
 }
 
