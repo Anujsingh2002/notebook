79284f11d922380bca2636e8d68d87f70e8e9443
Anuj Singh <108325005+Anujsingh2002@users.noreply.github.com>
2024-09-25 20:33:36 +0530

    Update Login.jsx
    host update

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -9,7 +9,7 @@ const Login = (props) => {
         e.preventDefault();
 
          //server side
-         const response = await fetch(`http://localhost:5000/api/auth/login`, {
+         const response = await fetch(`https://notebook-backend-23fi.onrender.com/api/auth/login`, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
